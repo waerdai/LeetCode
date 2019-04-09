4bfc1bfbf37e2c6eacc84debd66352ee21377a7c
weizhang <1065795573@qq.com>
2019-04-09 11:15:58 +0800

    new project

---
+++ LeetCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LeetCode
+//
+//  Created by wei zhang on 2019/4/9.
+//  Copyright © 2019 wei zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LeetCode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LeetCode
+//
+//  Created by wei zhang on 2019/4/9.
+//  Copyright © 2019 wei zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
